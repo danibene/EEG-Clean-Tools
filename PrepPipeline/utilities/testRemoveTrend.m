cd1490d77d99836516369769442ad1dc4cee29a5
danielle <dbenesch@critias.ca>
2022-10-31 09:57:01 -0400

    plots

--- PrepPipeline/utilities/testRemoveTrend.m
+++ PrepPipeline/utilities/testRemoveTrend.m
@@ -1,3 +1,6 @@
+%%
+% p = fileparts(pwd);
+% addpath(genpath(p));
 %% simulate signal
 duration = 120;
 frequency = [0.2, 0.4, 0.8, 1.6, 3.2];
@@ -12,14 +15,14 @@ for i=1:length(frequency)
     data = data + amplitude(i) * sin(2 * pi * frequency(i) * seconds);
 end
 %% confirm that signal was simulated as intended
-% figure
-% plot(data)
-% minFreq = 0.1;
-% window = round((2 / minFreq) * srate);
-% noverlap = round(window/2);
-% f = frequency;
-% figure
-% pwelch(data, window, noverlap, f, srate);
+figure
+plot(data)
+minFreq = 0.1;
+window = round((2 / minFreq) * srate);
+noverlap = round(window/2);
+f = frequency;
+figure
+pwelch(data, window, noverlap, f, srate);
 %% format input
 EEG.data = data;
 EEG.srate = srate;
@@ -29,15 +32,13 @@ detrendIn = struct('detrendChannels', 1, 'detrendType', 'linear', ...
 %%
 [EEG, detrendOut] = removeTrend(EEG, detrendIn);
 
-% Output:
-% testRemoveTrend
-% Unrecognized function or variable 'change_row_to_column'.
-% 
-% Error in localDetrend (line 35)
-% data = change_row_to_column(data);
-% 
-% Error in removeTrend (line 83)
-%         localDetrend(EEG.data(detrendOut.detrendChannels, :)', ...
-% 
-% Error in testRemoveTrend (line 30)
-% [EEG, detrendOut] = removeTrend(EEG, detrendIn);
+%% plot detrended data
+figure
+plot(EEG.data)
+minFreq = 0.1;
+window = round((2 / minFreq) * srate);
+noverlap = round(window/2);
+f = frequency;
+figure
+pwelch(EEG.data, window, noverlap, f, srate);
+% Frequencies below 0.5 Hz not fully attenuated
